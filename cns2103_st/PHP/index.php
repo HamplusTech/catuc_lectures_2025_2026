b4711f38725bad4f2831dd354fbb957f9889465b
HamplusTech <51116379+HamplusTech@users.noreply.github.com>
2025-12-07 18:09:43 +0100

    Add index.php for welcome page with links

---
+++ cns2103_st/PHP/index.php
@@ -0,0 +1,11 @@
+<?php
+require './includes/header.php';
+?>
+<h1><em>Welcome</em>, Please choose your destination</h1>
+<hr>
+<a href="./register.php">Register</a> &nbsp || &nbsp
+<a href="./login.php">Login</a>
+<hr>
+<?php
+include './includes/footer.php';
+?>
