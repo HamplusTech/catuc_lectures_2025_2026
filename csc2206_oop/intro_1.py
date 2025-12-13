3f90359ee2d5fba5da56a483b3102b4191cabb28
HamplusTech <51116379+HamplusTech@users.noreply.github.com>
2025-12-13 19:09:16 +0100

    Add a program to print user-provided names

---
+++ csc2206_oop/intro_1.py
@@ -0,0 +1,4 @@
+# A program to print any given name from the user
+print("A program to print any given name from the user")
+username = input("Please type any name for me to print\n")
+print("The name you typed is\n", username.capitalize())
